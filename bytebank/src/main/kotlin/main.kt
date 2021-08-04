c0d4372500071a94a95c461673b6197488e31713
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2021-08-03 22:50:31 -0300

    Conhecendo a Orientação a objetos, Implementando comportamentos e Escrevendo código mais idiomático ao kotlin

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,65 +1,133 @@
 fun main() {
+    println("Bem vindo ao Bytebank")
 
-    print("Bem vindo ao ByteBank")
+    val contaLucivaldo = Conta(titular = "Lucivaldo", numero = 1000)
+    contaLucivaldo.deposita(200.0)
 
-    //variáveis no kotlin
-    //var (variavel multável) e val (variavel imultavel)
+    val contaFran = Conta(numero = 1001, titular = "Fran")
+    contaFran.deposita(300.0)
 
-    //inferir tipo na variável no kotlin mesmo não inferindo o tipo ele entende pelo tipo
-    // de dado que está sendo atribuido.
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
 
+    println(contaLucivaldo.titular)
+    println(contaLucivaldo.numero)
+    println(contaLucivaldo.saldo)
+
+    println("depositando na conta do ALucivaldo")
+    contaLucivaldo.deposita(50.0)
+    println(contaLucivaldo.saldo)
+
+    println("depositando na conta da Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
+
+    println("sacando na conta do Lucivaldo")
+    contaLucivaldo.saca(250.0)
+    println(contaLucivaldo.saldo)
+
+    println("sacando na conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
+
+    println("saque em excesso na conta do Lucivaldo")
+    contaLucivaldo.saca(100.0)
+    println(contaLucivaldo.saldo)
+
+    println("saque em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.saldo)
+
+    println("Transferência da conta da Fran para o Lucivaldo")
+
+    if (contaFran.transfere(destino = contaLucivaldo, valor = 300.0)) {
+        println("Transferência sucedida")
+    } else {
+        println("Falha na transferência")
+    }
+
+    println(contaLucivaldo.saldo)
+    println(contaFran.saldo)
+}
+
+class Conta(
+    var titular: String,
+    val numero: Int
+) {
+    var saldo = 0.0
+        private set
+
+    fun deposita(valor: Double) {
+        if (valor > 0) {
+            this.saldo += valor
+        }
+    }
+
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            saldo -= valor
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.deposita(valor)
+            return true
+        }
+        return false
+    }
+}
+
+fun testaCopiasEReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta("João", 1002)
+    contaJoao.titular = "João"
+    var contaMaria = Conta("Maria", 1003)
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
+}
+
+fun testaLacos() {
     var i = 0
-    while(i < 5 ) {
-
+    while (i < 5) {
         val titular: String = "Lucivaldo $i"
         val numeroConta: Int = 1000 + i
         var saldo = i + 10.0
 
         println("titular $titular")
-        println("numero da conta $numeroConta")
+        println("número da conta $numeroConta")
         println("saldo da conta $saldo")
         println()
         i++
-
     }
 
-    /* FOR
-
-    for (i in 5 downTo 1 step 1) {
-
-
-            if(i == 4){
-            //podemos utilizar continue ou break dependendo da situação que quiser.
-            break
-            }
-
-
+    for (i in 5 downTo 1) {
         val titular: String = "Lucivaldo $i"
         val numeroConta: Int = 1000 + i
         var saldo = i + 10.0
 
-
-        // testa saldo neutro -> saldo = 0.0
-
-        // testa saldo negativo -> saldo -= 1000
-
-        //concatenação podemos usar (+ ou utilizar utilizar o string template $)
         println("titular $titular")
-        println("numero da conta $numeroConta")
+        println("número da conta $numeroConta")
         println("saldo da conta $saldo")
         println()
-        }
-     */
-
-        //Utilizando Estruturas Condicionais
-
-//    testacondicoes(saldo)
     }
+}
 
-fun testacondicoes(saldo: Double) {
-
-//IF
-
+fun testaCondicoes(saldo: Double) {
     if (saldo > 0.0) {
         println("conta é positiva")
     } else if (saldo == 0.0) {
@@ -68,13 +136,9 @@ fun testacondicoes(saldo: Double) {
         println("conta é negativa")
     }
 
-
-// Quando
-
     when {
         saldo > 0.0 -> println("conta é positiva")
         saldo == 0.0 -> println("conta é neutra")
         else -> println("conta é negativa")
     }
 }
-
